ae018a518d7e3b4d53d0f7841d7a19e971920d7e
Vinayak Garole <108934895+vinayakgarole@users.noreply.github.com>
2022-11-11 23:12:05 +0530

    Update Length.java

--- src/com/bridgelabz/Length.java
+++ src/com/bridgelabz/Length.java
@@ -4,6 +4,7 @@ public class Length {
 
     double length(double x1, double y1, double x2, double y2) {
 
-        return Math.sqrt((Math.pow((x2 - x1), 2) + (Math.pow((y2 - y1), 2))));
+        double result = Math.sqrt((Math.pow((x2 - x1), 2) + (Math.pow((y2 - y1), 2))));
+        return result;
     }
 }
